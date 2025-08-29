9b67d948482d420a04a23b4f522ab4950ad27b0d
Lucas Ferron <lucasferron06@gmail.com>
2025-08-29 14:09:18 -0300

    Initial commit

---
+++ resources/views/aluno/list.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Imma be fera na programattion</p>
+    <p>I love code!</p>
+    <table>
+        <thead>
+            <tr>
+                <td>#ID</td>
+                <td>Nome</td>
+                <td>CPF</td>
+                <td>telefone</td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($dados as $item)
+                <tr>
+                    <td>{{$item->id}}</td>
+                    <td>{{$item->nome}}</td>
+                    <td>{{$item->cpf}}</td>
+                    <td>{{$item->telefone}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
+
+
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlunoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/aluno', [AlunoController::class, 'index']);
+/*
+Route::get('/aluno', function () {
+    return view('aluno.list');
+});
+*/
