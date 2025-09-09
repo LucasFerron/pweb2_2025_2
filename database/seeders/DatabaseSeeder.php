b160ea882d5d40e8043d35b10e6684cbed29f117
Lucas Ferron <lucasferron06@gmail.com>
2025-09-09 17:18:57 -0300

    Criação de seeds e factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
+        $this->call([
+            CategoriaAlunoSeeder::class,
+            AlunoSeeder::class,
+
+        ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
